3e5e4f564c431694f09260e8d1758d198cc34327
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-13 22:32:30 +0900

    contextでの再レンダリング contextでの再レンダリングを最適化

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
@@ -18,10 +21,15 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
+
   return (
     <Scontainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUSerArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
